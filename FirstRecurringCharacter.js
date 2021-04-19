712f9fc22463e2029e4a09aaf271229540785ddc
Karthik <kbalasub@stevens.edu>
2021-04-19 05:36:11 -0500

    Bonus Solution in First Recurring Character

--- FirstRecurringCharacter.js
+++ FirstRecurringCharacter.js
@@ -21,4 +21,16 @@ function firstRecurringCharacter(input) {
     return undefined;
 }
 
-console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]))
+function bonus(input) {
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i - 1; j >= 0; j--) {
+            if (input[i] === input[j]) {
+                return input[i];
+            }
+        }
+    }
+    return undefined
+}
+
+// console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]))
+console.log(bonus([2, 5, 1, 2, 3, 5, 1, 2, 4]))
