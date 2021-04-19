f46b31d71534f8ee676a104d1a59ac84382d007c
Karthik <kbalasub@stevens.edu>
2021-04-19 03:47:04 -0500

    Intro to Objects

---
+++ index.js
@@ -0,0 +1,12 @@
+let user = {
+    age: 54,
+    name: 'Karu',
+    magic: true,
+    scream: function () {
+        console.log("Ahhhhhhhh!")
+    }
+}
+
+console.log(user.scream())
+user.spell = 'Hokus Pokus'
+console.log(user)
